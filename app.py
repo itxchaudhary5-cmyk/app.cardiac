443ad27a7ff010e2ff5a79a3a1969ba350833c94
itxchaudhary5-cmyk <itxchaudhary5@gmail.com>
2026-09-04 00:34:48 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("🫀 Cardiac Care App")
+
+st.write("Welcome to our Heart Health Awareness App ❤️")
+
+st.warning(
+    "This app is for educational purposes only. "
+    "It does not diagnose or replace professional medical care."
+)
